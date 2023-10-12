94c2a0ac2350f0732115490cc4456cc3d370a851
Roberta Stumpf <robertamdstumpf@gmail.com>
2023-10-12 20:55:19 -0300

    Projeto concluído

--- aluramidi-curso/main.js
+++ aluramidi-curso/main.js
@@ -1,8 +1,37 @@
-function tocaSomPom() {
 
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idAudio) {
+
+    document.querySelector(idAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+//Armazena todos elementos de classe "tecla" em listaDeTeclas
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    //Representa a tecla atual
+    const tecla = listaDeTeclas[contador];
+
+    //O classList acessa todas as classes de um elemento.
+    //Neste caso, pegamos a segunda classe de índice 1 que diz o nome
+    //da tecla. 
+
+    //ex: instrumento = tecla_pom
+    const instrumento = tecla.classList[1];
+
+    //Acessamos o id 
+    //ex: idAudio = #som_ + tecla_pom
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+}
+
+
 
 
